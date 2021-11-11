9e9bf89fed08b2f096951aa87f42ce4b520ebea7
tonimatias <tonimatias96@gmail.com>
2021-11-11 12:39:24 +0200

    lisäyksiä

---
+++ Advanced JavaScript/3.harjoitus classlist/code.js
@@ -0,0 +1,8 @@
+let button = document.querySelector("button")
+
+button.addEventListener('click', buttonClicked)
+
+function buttonClicked(){
+    let div = document.querySelector('div')
+    button.classList.toggle.div = ("style", "background-color = blue")
+}
